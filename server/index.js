c208e1abe300f104ad12cff9d51fb85d32ab5084
tyymr23 <tmrobertson2323@gmail.com>
2023-07-26 13:53:18 -0400

    user login authentication setup

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "/.controllers/auth.js"
+import authRoutes from "./routes/auth.js";
+import { register } from "/.controllers/auth.js";
 
 // CONFIGS
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +38,9 @@ const storage = multer.diskStorage({
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
