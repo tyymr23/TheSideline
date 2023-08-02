4807a306bc9ec1a1ddf882ebcd7e116ea20c2e55
tyymr23 <tmrobertson2323@gmail.com>
2023-08-02 15:24:22 -0400

    finished backend portion

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
             picturePath,
             friends,
             location,
-            occupation
+            occupation,
         } = req.body;
 
         const salt = await bcrypt.genSalt();
@@ -29,7 +29,7 @@ export const register = async (req, res) => {
             location,
             occupation,
             viewedProfile: Math.floor(Math.random() * 10000),
-            impressions: Math.floor(Math.random() * 10000)
+            impressions: Math.floor(Math.random() * 10000),
         });
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
@@ -43,10 +43,10 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email });
-        if (!user) return res.status(400).json({ msg: "User does not exist." });
+        if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         delete user.password;
@@ -54,4 +54,4 @@ export const login = async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
-}
+};
--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { register } from "/.controllers/auth.js";
+import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGS
 const __filename = fileURLToPath(import.meta.url);
@@ -27,18 +30,18 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // sets directory of assets
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// FILE STORAGE (this is from multer guithub repo)
+// FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  const upload = multer({ storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
@@ -59,7 +62,7 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
+    // ADD DATA ONE TIME
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
