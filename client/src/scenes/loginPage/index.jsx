dcef0e70f8d0f822d8a484f8597797b9add2f3d9
tyymr23 <tmrobertson2323@gmail.com>
2023-08-04 10:38:17 -0400

    added login page functionality

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,33 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-    return (<div>loginpage</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-widthL 1000px)");
+    return <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >
+                Connect+
+            </Typography>
+        </Box>
+
+        <Box 
+            width={isNonMobileScreens ? "50%" : "93%"} 
+            p="2rem" 
+            m="2rem auto" 
+            borderRadius="1.5rem" 
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
+                Welcome to Connect+
+            </Typography>
+            <Form />
+        </Box>
+    </Box>
 }
 
 export default LoginPage;
